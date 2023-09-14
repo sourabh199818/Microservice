5fae9af309a820ddc2e03f2a908212b25d3ab136
Sourabh Bisht <68646654+sourabh199818@users.noreply.github.com>
2023-09-14 18:09:47 +0530

    Add files via upload

---
+++ inventory-service/src/main/java/com/sourabhb/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.sourabhb.inventoryservice.repository;
+
+import com.sourabhb.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
